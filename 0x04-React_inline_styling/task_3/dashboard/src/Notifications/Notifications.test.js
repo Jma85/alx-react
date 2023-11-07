57eb0612f18105caa8d50aada2aa285e41cb2751
Jma85 <jma24834@gmail.com>
2023-11-07 18:09:50 +0300

    When the panel is open, it should take over the entire screen There should be no padding because of the ul element The font size of the text should be 20px

---
+++ 0x04-React_inline_styling/task_3/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { getLatestNotification } from "../utils/utils";
+import Notifications from "./Notifications";
+import NotificationItem from "./NotificationItem";
+import { StyleSheetTestUtils } from "aphrodite";
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
